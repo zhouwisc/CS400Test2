8a3b9bd709d14486cf748a10870764ebc1af70c0
zhouwisc <jzhou346@wisc.edu>
2024-06-12 12:14:08 -0500

    New changes to print out

--- Printout.java
+++ Printout.java
@@ -1,6 +1,6 @@
 // A test file for printing out a sentence
 public class Printout {
 	public static void main(String [] args){
-		System.out.println("Just a test");
+		System.out.println("Just a test and more!");
 	}
 }
